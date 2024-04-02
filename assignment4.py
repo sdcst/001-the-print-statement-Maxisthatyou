3baa54c2b1b2aeb6a14548adf7d63af645122500
Maxisthatyou <158555539+Maxisthatyou@users.noreply.github.com>
2024-04-02 13:15:10 -0700

    yep

--- assignment4.py
+++ assignment4.py
@@ -1,6 +1,5 @@
 #!python3
 
-Modify this program using comment structures so that
-only the print statement is executed.
+#Modify this program using comment structures so that only the print statement is executed.
 
 print("Hello!")
